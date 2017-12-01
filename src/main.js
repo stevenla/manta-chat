59ae7c1f5af7d7843fbedbb1b618aacd0a71ea55
Steven La <mrstevenla@gmail.com>
2017-12-01 15:10:45 -0500

    remove high perf

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import PluginManager from './PluginManager';
 
 const plugins = new PluginManager('main');
 
+app.disableHardwareAcceleration();
 app.on('ready', () => {
   plugins.load();
   plugins.emit('init');
