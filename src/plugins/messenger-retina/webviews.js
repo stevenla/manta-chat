33ef0dd2a43b7a761856609a5910dbcbacb86fff
Steven La <mrstevenla@gmail.com>
2018-01-10 16:04:21 -0800

    facebook retina

--- src/plugins/messenger-retina/webviews.js
+++ src/plugins/messenger-retina/webviews.js
@@ -1,10 +1,11 @@
+import Cookie from 'js-cookie';
 /**
  * Manually set device pixel ratio to 2x
  */
 export default (plugins) => {
   plugins.on('load', () => {
-    if (location.host.match(/messenger\.com/)) {
-      window.setCookie('dpr', '2');
+    if (location.host.match(/messenger\.com/) || location.host.match(/facebook\.com/)) {
+      Cookie.set('dpr', '2');
     }
   });
 }
