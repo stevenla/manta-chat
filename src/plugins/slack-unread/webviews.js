5903234c76985ed42fedc1c804a5d7481b9dc2a0
Steven La <mrstevenla@gmail.com>
2018-07-13 17:12:39 -0700

    Fixed slack badging

--- src/plugins/slack-unread/webviews.js
+++ src/plugins/slack-unread/webviews.js
@@ -5,7 +5,10 @@ export default (plugins) => {
   plugins.on('load', () => {
     if (location.host.match(/slack\.com/)) {
       setInterval(() => {
-        const allUnreadCount = document.querySelectorAll('.section_holder .unread').length;
+        const allUnreadCount = Array.prototype.filter.call(
+          document.querySelectorAll('.p-channel_sidebar a'),
+          item => item.className.includes('unread') && !item.className.includes('muted')
+        ).length;
         ipcRenderer.sendToHost('unread', allUnreadCount);
       }, REFRESH_INTERVAL);
     }
